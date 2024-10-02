e14da050751b2c6bd5c8530a7af8e6d8b0abea95
waqarAli <alisyedwaqar108@gmail.com>
2024-10-02 14:30:21 +0500

    better comments in scopes

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -1,10 +1,10 @@
-// //never use var because it has a global scope , and can produce confusions in code execution
+// *never use var because it has a global scope , and can produce confusions in code execution
 
 // let a = 10;
 // var b = 20;
 // const c = 30;
 
-// // if we redeclare let and const inside some function or loop scope , their global value is not going to be affected
+// *if we redeclare let and const inside some function or loop scope , their global value is not going to be affected
 // if (true) {
 //   let a = 60;
 //   var b = 77;
