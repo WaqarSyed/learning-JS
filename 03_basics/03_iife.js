608c91c971bd1d643b983d8efe890ae37fb1082a
waqarAli <alisyedwaqar108@gmail.com>
2024-10-05 03:08:44 +0500

    IIFE using arrow function syntax

--- 03_basics/03_iife.js
+++ 03_basics/03_iife.js
@@ -10,7 +10,22 @@
 // }
 // dbConnection();
 //! following is another syntax for defining and calling an IIFE , right after its definition
-(function dbConnection() {
-  console.log("DB connected");
-})(/*these parenthesis are used to call the function after definition */);
+// (function dbConnection() {
+//   console.log("DB connected");
+// })(/*these parenthesis are used to call the function after definition */);
 //* above both standard methods of writing functions are also known as named IIFE
+//? how we can pass arguments to an IIFE
+// (function dbConnection(name) {
+//   console.log(`DB connected ${name}`);
+// })("waqar");
+//! we can pass arguments in the parenthesis , being used for calling the function
+//* we can define IIFE using arrow functions like below
+((name) => {
+  console.log(`DB connected ${name}`);
+})(
+  "shah g" /*these parenthesis are used to call the function after definition */
+);
+//! we can pass arguments to arrow function IIFE , in the same way we pass to standard method
+
+//! we can declare two or more IIFEs in the same file but each IIFE call must end with a semicolon
+//! IIFE defined  using arrow function method are also known as unnamed IIFE
