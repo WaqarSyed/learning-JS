367b17fa8a27ea0f6609074e6a4c6ee59db0be09
waqarAli <alisyedwaqar108@gmail.com>
2024-10-05 02:56:34 +0500

    IIFE, named IIFE

--- 03_basics/03_iife.js
+++ 03_basics/03_iife.js
@@ -1,1 +1,16 @@
 //? IMEDIATELY INVOKED FUNCTION EXPRESSIONs (IIFE)
+/*IIFE is function that needs to be executed right after its definition , e.g: we have a separate file for database
+ connection. And we want the database connection to established as our application loads , for this purpose IIFE 
+ is best method to be utilized */
+//? how we define an IIFE
+// * following is the syntax for writing an iife and calling it , right after its definition
+
+// function dbConnection() {
+//   console.log("DB connected");
+// }
+// dbConnection();
+//! following is another syntax for defining and calling an IIFE , right after its definition
+(function dbConnection() {
+  console.log("DB connected");
+})(/*these parenthesis are used to call the function after definition */);
+//* above both standard methods of writing functions are also known as named IIFE
