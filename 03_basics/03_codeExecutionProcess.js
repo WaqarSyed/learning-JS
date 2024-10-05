b8f1a59a520cfc9bba377a5b40d35ecd79b83141
waqarAli <alisyedwaqar108@gmail.com>
2024-10-06 04:19:34 +0500

    phases in code execution

--- 03_basics/03_codeExecutionProcess.js
+++ 03_basics/03_codeExecutionProcess.js
@@ -1,1 +1,8 @@
 //? How does javascript executes code + call stack
+//!Here are the phases in sequence:
+
+//* Parsing (Creation Phase)
+//* Compilation (JIT - Just-In-Time Compilation)
+//* Execution (Execution Phase)
+//* Event Loop & Asynchronous Handling
+//* Garbage Collection (Memory Management)
