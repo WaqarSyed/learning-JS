e9bd3e16699ae4cc681b18e10e7cb8ec8d8a8a15
waqarAli <alisyedwaqar108@gmail.com>
2024-10-07 15:53:40 +0500

    understand call stack

--- 03_basics/03_codeExecutionProcess.js
+++ 03_basics/03_codeExecutionProcess.js
@@ -14,7 +14,8 @@ function addNum(num1, num2) {
   return total;
 }
 let result1 = addNum(val1, val2);
-
+console.log(result1);
+console.log(result1);
 //TODO1: Global execution context is created and then referred to 'this' variable
 //TODO2: Memory Creation Phase/ Creation Phase :
 //* all the variables are assigned memory location but not initialized(means that :
@@ -28,3 +29,11 @@ let result1 = addNum(val1, val2);
 //! a new varibale environment + execution thread is created
 //! this environment is particularly for that function and it consists of further two sub phases
 //* Memory phase and Execution phase
+//? what happens when a function is executed successfully
+//* new variable environment + execution thread are deleted for better memory utilization
+//? What is call Stack
+//* call stack is responsible for managing the execution of function calls, It operates as a 'LAST IN , FIRST OUT' data structure
+//!meaning the last function called is the first one to be executed and resolved.
+
+//? enlist fundamental responsibilies of the call stack
+//* Tracking function calls, Managing Execution Contexts, Handling function execution order, Error Handling
