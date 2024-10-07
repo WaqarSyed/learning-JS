b6c7e929d8e1bcc9eb145e796ed2ee8266a80e2c
waqarAli <alisyedwaqar108@gmail.com>
2024-10-07 15:08:35 +0500

    write example code to understand execution

--- 03_basics/03_codeExecutionProcess.js
+++ 03_basics/03_codeExecutionProcess.js
@@ -6,3 +6,11 @@
 //* Execution (Execution Phase)
 //* Event Loop & Asynchronous Handling
 //* Garbage Collection (Memory Management)
+//! let's understand the process by simulating an example:
+let val1 = 10;
+let val2 = 5;
+function addNum(num1, num2) {
+  let total = num1 + num2;
+  return total;
+}
+let result1 = addNum(val1, val2);
