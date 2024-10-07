675b5f96bc0f5069f8c8b4e170d2496b73925740
waqarAli <alisyedwaqar108@gmail.com>
2024-10-07 15:26:00 +0500

    Global execution context,Memory Creation Phase , Execution phase

--- 03_basics/03_codeExecutionProcess.js
+++ 03_basics/03_codeExecutionProcess.js
@@ -14,3 +14,12 @@ function addNum(num1, num2) {
   return total;
 }
 let result1 = addNum(val1, val2);
+
+//TODO1: Global execution context is created and then referred to 'this' variable
+//TODO2: Memory Creation Phase/ Creation Phase :
+//* all the variables are assigned memory location but not initialized(means that :
+//! just varibles are allocated with memory but not assigned values, and by default they all hold 'undefined')
+//* function name holds its definition
+//TODO3: Funtion Execution Phase
+//* when function call appears as in line 16 we are inoking the addNum function and holding its return value
+//* inside a variable
