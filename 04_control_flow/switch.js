2d722f29e2707f7254763e98637328cd57b68996
waqarAli <alisyedwaqar108@gmail.com>
2024-10-09 18:48:01 +0500

    switch and break

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,68 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+//* to copy lines of code press (Shift + Alt +down arrow, up arrow)
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("Month is January");
+
+    break;
+  case 2:
+    console.log("Month is February");
+
+    break;
+  case 3:
+    console.log("Month is March");
+
+    break;
+  case 4:
+    console.log("Month is April");
+
+    break;
+  case 5:
+    console.log("Month is May");
+
+    break;
+  case 6:
+    console.log("Month is June");
+
+    break;
+  case 7:
+    console.log("Month is July");
+
+    break;
+  case 8:
+    console.log("Month is August");
+
+    break;
+  case 9:
+    console.log("Month is September");
+
+    break;
+  case 10:
+    console.log("Month is October");
+
+    break;
+  case 11:
+    console.log("Month is November");
+
+    break;
+  case 12:
+    console.log("Month is December");
+
+    break;
+
+  default:
+    console.log("enter the month 1-12");
+
+    break;
+}
+/* //!When the break statement is encountered, the control is transferred out of the current block or loop, 
+and the program execution resumes at the next statement following the loop or switch. */
