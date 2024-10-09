9c8e3a1b9d58e824c17e10ebe993d461db77c0a2
waqarAli <alisyedwaqar108@gmail.com>
2024-10-09 17:05:16 +0500

    comparison operators

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,2 +1,9 @@
 /* Control flow refers to the order in which statements are executed in a program, which is typically from the top
 to bottom of the Code */
+// * However , using control flow structures, such as conditionals & loops, we can change the way a program flows
+let isUserLoggedIn = true;
+
+if (isUserLoggedIn) {
+  console.log("Welcome Back");
+}
+//! <, > ,<=, >=, ==, ===, !=, !== , These are common comparison operators
