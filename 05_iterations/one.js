020206a56dd6afbb417f1ac4245a5c91e545fc85
waqarAli <alisyedwaqar108@gmail.com>
2024-10-11 03:48:11 +0500

    break and continue keywords in javascript

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -7,10 +7,27 @@
 //   }
 //   console.log(element);
 // }
-
-for (let i = 1; i <= 10; i++) {
+//! shift + alt + A is shortcut for toggle block comment
+/* for (let i = 1; i <= 10; i++) {
   for (let j = 1; j <= 10; j++) {
     console.log(`${i} * ${j} = ${i * j} `);
   }
   console.log(`table of ${i} is completed upto 10 `);
+} */
+// const myArray = ["batman", "superman", "spiderman"];
+// console.log(myArray.length);
+
+// for (let index = 0; index < myArray.length; index++) {
+//   const element = myArray[index];
+//   console.log(element);
+// }
+// ? break and continue
+for (let i = 1; i <= 20; i++) {
+  if (i == 5) {
+    console.log(`Detected 5 `);
+    continue;
+  }
+  console.log(i);
 }
+//* break basically breaks the flow and take out the containing flow
+//* continue neglects the current iteration
