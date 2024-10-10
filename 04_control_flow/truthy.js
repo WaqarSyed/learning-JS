891c52a734bda8d79e04c6abe404ef06a58e808d
waqarAli <alisyedwaqar108@gmail.com>
2024-10-10 15:53:38 +0500

    Nullish Coalescing Operator is safety check for null & undefined values

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -26,8 +26,29 @@
 //   console.log("No user found");
 // }
 //? note the following for interviews , competitive programming that
-console.log(false == 0);
-console.log(false == "");
+// console.log(false == 0);
+// console.log(false == "");
 
-console.log(0 == "");
+// console.log(0 == "");
 //! all of the above 3 scenarios return true
+
+//* Logical operators are '&&' and '||'
+//!  && returns true when all of the condition checks are true
+//! || returns true when at least of the condition check is true
+
+//? Nullish Coalescing Operator (??)
+//* Basically , it's safety check for null and undefined values, whenever we fetch data from an API or DB Query
+//* it may result in null or undefined so , that operator perform safety checks , let's have a look at example code
+// let val1 = 23 ?? 7;
+//! above code line will assign 23 to val1
+//* now below line will assign 7 , so it's checking to avoid null or undefined to be assigned
+// let val1 = null ?? 7;
+// console.log(val1);
+
+let val1 =
+  null ??
+  undefined ??
+  (function num() {
+    return 4;
+  })();
+console.log(val1);
