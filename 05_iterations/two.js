5da3b05e8a1fe3a1c58ccc4c50cd08f87263acaa
waqarAli <alisyedwaqar108@gmail.com>
2024-10-12 01:19:07 +0500

    do while loop

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,5 +1,17 @@
-let index = 0;
+/* let index = 0;
 while (index <= 10) {
   console.log(` value of index is ${index}`);
   index += 2;
 }
+ */
+// const myArray = ["flash", "batman", "superman"];
+// let arr = 0;
+// while (arr < myArray.length) {
+//   console.log(` value is ${myArray[arr]}`);
+//   arr += 1;
+// }
+let score = 1;
+do {
+  console.log(` score is ${score}`);
+  score++;
+} while (score <= 5);
