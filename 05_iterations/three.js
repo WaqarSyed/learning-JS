eb2f94f347fa0700f5c4f95b03e8f1d717b8f147
waqarAli <alisyedwaqar108@gmail.com>
2024-10-14 02:46:43 +0500

    maps hold unique values

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,5 +1,23 @@
-// for of loop
-const arr = [1, 2, 3, 4, 5];
-for (const element of arr) {
-  console.log(element);
+//? for of loop
+// const arr = [1, 2, 3, 4, 5];
+// for (const element of arr) {
+//   console.log(element);
+// }
+
+//* this loop can be used with any iterable object/array/string
+/* const greetings = "hello world";
+for (const greet of greetings) {
+  if (greet == "w") break;
+  console.log(greet);
+} */
+
+// Maps:
+//! maps are mostly like objects, but maps remember the order of insertion and hold unique values
+const map = new Map();
+map.set("PK", "Pakistan");
+map.set("FR", "France");
+map.set("IN", "India");
+map.set("IR", "Iran");
+for (const [key, value] of map) {
+  console.log(key, value);
 }
