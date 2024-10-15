bbfd8f4b0896ae8d10d26f30e2b02e16c4cc09fb
waqarAli <alisyedwaqar108@gmail.com>
2024-10-16 01:22:30 +0500

    forEach method returns undefined

---
+++ 05_iterations/six.js
@@ -0,0 +1,6 @@
+const coding = ["js", "ruby", "python", "html", "css"];
+const values = coding.forEach((item) => {
+  console.log(item);
+});
+console.log(values);
+//* So, forEach returns "undefined" means that it returns nothing
