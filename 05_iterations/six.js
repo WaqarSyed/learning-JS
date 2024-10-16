7a8a75bbd4b3991ad5aab58c38fb1c965447830f
waqarAli <alisyedwaqar108@gmail.com>
2024-10-17 01:33:12 +0500

    filter method with curly braces which needs a return statement

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,6 +1,16 @@
-const coding = ["js", "ruby", "python", "html", "css"];
-const values = coding.forEach((item) => {
-  console.log(item);
-});
-console.log(values);
+// const coding = ["js", "ruby", "python", "html", "css"];
+// const values = coding.forEach((item) => {
+//   console.log(item);
+// });
+// console.log(values);
 //* So, forEach returns "undefined" means that it returns nothing
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newNums = myNums.filter((num) => num > 4);
+// console.log(newNums);
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const newNums = myNums.filter((num) => {
+  num > 4;
+});
+console.log(newNums);
+//! in the above code snippet , we are using '{}' and they need a must have return statement
