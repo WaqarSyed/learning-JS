7dbbf24fa61024f257ccc033324fb95cd0545637
waqarAli <alisyedwaqar108@gmail.com>
2024-10-18 21:46:35 +0500

    practice to handle db response

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -8,9 +8,25 @@
 // const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // const newNums = myNums.filter((num) => num > 4);
 // console.log(newNums);
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+/* const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const newNums = myNums.filter((num) => {
   num > 4;
 });
-console.log(newNums);
+console.log(newNums); */
 //! in the above code snippet , we are using '{}' and they need a must have return statement
+//! So , whenever we use curly braces : it means we are opening a new scope and each scope must include a return statement
+//? how to handle data which comes from API response? let's look at the following example
+const books = [
+  { title: "Book One", genre: "Fiction", publish: 1981, edition: 2004 },
+  { title: "Book Two", genre: "Non-Fiction", publish: 1992, edition: 2008 },
+  { title: "Book Three", genre: "History", publish: 1999, edition: 2007 },
+  { title: "Book Four", genre: "Non-Fiction", publish: 1989, edition: 2010 },
+  { title: "Book Five", genre: "Sience", publish: 2009, edition: 2014 },
+  { title: "Book Six", genre: "Fiction", publish: 1987, edition: 2010 },
+  { title: "Book Seven", genre: "History", publish: 1986, edition: 1996 },
+];
+
+const userBooks = books.filter(
+  (bk) => bk.genre == "History" && bk.publish > 1990
+);
+console.log(userBooks);
