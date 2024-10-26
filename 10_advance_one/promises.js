cea65d001995af315007455612eb1266104394fd
waqarAli <alisyedwaqar108@gmail.com>
2024-10-26 21:17:54 +0500

    promises has two phases : first is creation and second is consumption

---
+++ 10_advance_one/promises.js
@@ -0,0 +1,5 @@
+// The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+/* A promise is in one of these states:
+ * pending: neither fulfilled nor rejected.
+ * completed: meaning that the operation was completed successfully.
+ * rejected: meaning that the operation failed. */
