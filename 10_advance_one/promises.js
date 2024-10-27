3819921fba62b7feb20134ddaeab25d211c7482e
waqarAli <alisyedwaqar108@gmail.com>
2024-10-27 23:00:50 +0500

    passing data to .then from promise consumption :: promiseThree

--- 10_advance_one/promises.js
+++ 10_advance_one/promises.js
@@ -11,5 +11,27 @@ const promiseOne = new Promise(function (resolve, reject) {
   //DB calls, cryptography, network calls
   setTimeout(() => {
     console.log("Async task is complete");
+    resolve();
   }, 1000);
+});
+promiseOne.then(function () {
+  console.log("Promise Consumed");
+});
+
+new Promise((resolve, reject) => {
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(() => {
+  console.log("task 2 resolved");
+});
+
+const promiseThree = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({ name: "Syed Waqar", email: "alisyedwaqar108@gmail.com" });
+  }, 1000);
+});
+promiseThree.then((user) => {
+  console.log(`Name : ${user.name} Email: ${user.email} `);
 });
