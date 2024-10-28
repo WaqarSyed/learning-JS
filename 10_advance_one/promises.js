b21c17c79183b6a859804f5eff299fc6f8cdf428
waqarAli <alisyedwaqar108@gmail.com>
2024-10-28 17:35:59 +0500

    under the hood working of the fetch api discussed in comments

--- 10_advance_one/promises.js
+++ 10_advance_one/promises.js
@@ -6,23 +6,23 @@
 //! Note that before consuming promises we must be able to create promises
 //* study Q and Bluebird in detail
 
-/* const promiseOne = new Promise(function (resolve, reject) {
-  //Do an Async task
-  //DB calls, cryptography, network calls
-  setTimeout(() => {
-    console.log("Async task is complete");
-    resolve();
-  }, 1000);
-});
-promiseOne.then(function () {
-  console.log("Promise Consumed");
-});
+// const promiseOne = new Promise(function (resolve, reject) {
+//   //Do an Async task
+//   //DB calls, cryptography, network calls
+//   setTimeout(() => {
+//     console.log("Async task is complete");
+//     resolve();
+//   }, 1000);
+// });
+// promiseOne.then(function () {
+//   console.log("Promise Consumed");
+// });
 
 new Promise((resolve, reject) => {
   setTimeout(function () {
     console.log("Async task 2");
     resolve();
-  }, 1000);
+  }, 2000);
 }).then(() => {
   console.log("task 2 resolved");
 });
@@ -30,11 +30,11 @@ new Promise((resolve, reject) => {
 const promiseThree = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve({ name: "Syed Waqar", email: "alisyedwaqar108@gmail.com" });
-  }, 1000);
+  }, 2000);
 });
 promiseThree.then((user) => {
   console.log(`Name : ${user.name} Email: ${user.email} `);
-}); */
+});
 
 /* const promiseFour = new Promise((resolve, reject) => {
   setTimeout(() => {
@@ -108,3 +108,19 @@ fetch("https://api.github.com/users/waqarSyed")
   .catch((error) => {
     console.log(error);
   });
+//! Following are the important things to know about fetch() API
+//* The global fetch method starts the process of fetching a resource from the network , returning a promise which is
+//*fulfilled once the response is available.
+//? Interview Question : Suppose you made request using (fetch) promise and it came with a ERROR CODE: 404, how can get hold of it (either in resolve
+//? or reject)
+//* we will get that in resolve because it's response from the server that we are tapping. reject only triggers when
+//* we are unable to make the request
+//! it means our request don't know the meaning of status codes.
+/*
+A fetch() promise only rejects when a network error is encountered (which is usually when there is permissions issue 
+or similar). A fetch() promise does not reject on HTTP errors (404, etc). Instead a then() handler must check the 
+Response.ok and/or Response.status properties.
+! Whenever we make a fetch request : it executes two primary tasks : 1:: to make the memory for upcoming Data and makes
+! some private fields (Data, onFulfilled[], onRejection[]) , also referred as (Data, Resolve, Reject)
+! 2:: makes the network request using node/Web-browser and this request is either resolved or rejected
+*/
